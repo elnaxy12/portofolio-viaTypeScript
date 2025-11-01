63199b55e38a169d87a29a40f605c4c369b0dfeb
elnaxy12 <aryagilang533@gmail.com>
2025-11-01 19:53:43 +0700

    git init

--- app/page.tsx
+++ app/page.tsx
@@ -182,7 +182,10 @@ export default function Home(): JSX.Element {
             {/* ============ BATAS ANIMASI SCROLL FADE =============== */}
 
             {/* CONTENT HISTORY PROJECT */}
-            <div className="w-full container flex flex-col justify-center">
+            <div
+              id="project"
+              className="w-full container flex flex-col justify-center"
+            >
               <SplitText
                 text="Biggest Project"
                 className="text-4xl text-white font-semibold text-center h-[50px]"
@@ -204,7 +207,7 @@ export default function Home(): JSX.Element {
               </p>
             </div>
 
-            <div id="project" className="container h-screen flex flex-col mb-10">
+            <div className="container h-screen flex flex-col mb-10">
               {/* ANIMASI SCROLL FADE */}
               <ScrollFloat
                 animationDuration={1}
